cb7c4a6beddb14314bd470e58491868c164f5ff6
ArthurGSO <arthurprobr422@gmail.com>
2023-09-06 20:00:22 -0300

    3 funções

--- meuphp.php
+++ meuphp.php
@@ -1,14 +1,42 @@
-<?php 
+<?php
+	
+	//criando função
 
-	function somaValor($num1,$num2){
+	function somaValor(){
+
+		$num1 = 34;
+		$num2 = 46;
+		$resp = $num1 + $num2 ;
+
+		echo "O valor da soma é ".$resp;
+	} 
+
+	somaValor(); //chamando função
+
+
+
+	//criando função
+
+	function somaNumeros($num1,$num2){
 
 		$resp = $num1 + $num2;
 
-		echo "O valor da soma é ".$resp;
+		echo "<br><br>O valor da soma é ".$resp;
 	}
 
-	somaValor(10.30,50.30);
+	somaNumeros(10.30,50.30); //chamando função
 
-	echo "git";
+
+
+
+	//criando função
+
+	function somaNum($num1, $num2){
+
+		return ($num1+$num2);
+
+	}
+
+	echo "<br><br>A soma dos números é " . somaNum(20.35,80,50); //chamando função
 
  ?>
